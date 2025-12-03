cae5e714670c4e7b2969e071bc7fc97a753f6d35
Diego Landa <dlandat95@gmail.com>
2025-12-02 19:55:20 -0500

    Feat: Implementación completa de Auth (Login, Registro, Logout y Guard)

--- src/app/shared/components/layout/topbar/topbar.ts
+++ src/app/shared/components/layout/topbar/topbar.ts
@@ -1,8 +1,12 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component,Input, Output, EventEmitter , inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+
+import { AuthService } from '../../../../core/auth/auth.service';
+
+
 @Component({
   selector: 'app-topbar',
   imports: [CommonModule, MatIconModule, MatButtonModule, MatBadgeModule],
@@ -12,5 +16,13 @@ import { MatBadgeModule } from '@angular/material/badge';
 export class Topbar {
  @Input() title: string = 'Dashboard';
   @Output() menuClick = new EventEmitter<void>(); // Emitimos el evento al padre 
+
+   // Inyectamos el servicio
+  private authService = inject(AuthService);
+
+   onLogout() {
+    // Llamamos al método logout que ya limpia el localStorage y redirige
+    this.authService.logout();
+  }
 
 }
