154ec0cdfcef1b6228c449fd3d62762f6a055962
Diego Landa <dlandat95@gmail.com>
2026-01-03 02:09:20 -0500

    mejora-componentes-personalizados 2

--- src/app/features/home/dashboard/views/dashboard-view/dashboard-view.ts
+++ src/app/features/home/dashboard/views/dashboard-view/dashboard-view.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { WelcomeBanner } from '../../components/welcome-banner/welcome-banner';
 import { KpiWidget } from '../../components/kpi-widget/kpi-widget';
 import { ClockWidget } from '../../components/clock-widget/clock-widget';
+import { ContentLayoutView } from '@shared/components/layout/content-layout-view/content-layout-view';
+import { CustomButton } from '@shared/components/custom-button/custom-button';
+
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-dashboard-view',
-  imports: [CommonModule, WelcomeBanner, KpiWidget,ClockWidget],
+  imports: [CommonModule, WelcomeBanner,ContentLayoutView , CustomButton,KpiWidget,ClockWidget,MatIcon],
   templateUrl: './dashboard-view.html',
   styleUrl: './dashboard-view.scss',
 })
